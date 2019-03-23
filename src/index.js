99d2e807025218e5eb231b76831206bad3149814
Grzegorz Kolek <gkolek@gazeta.pl>
2019-03-23 09:24:12 +0100

    my 1st redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const counterReducer = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT_COUNTER':
+            return state + 1
+        default:
+            return state
+    }
+}
+
+let store = createStore(counterReducer)
+console.log('state initial', store.getState())
+store.dispatch({
+    type: 'INCREMENT_COUNTER'
+})
+console.log('state after dispatch', store.getState())
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
