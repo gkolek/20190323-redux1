c864ea1dc54c468de18fd5909d5b96c7ace7c114
Grzegorz Kolek <gkolek@gazeta.pl>
2019-03-23 10:35:21 +0100

    actions

--- src/index.js
+++ src/index.js
@@ -5,35 +5,40 @@ import { createStore } from 'redux'
 import './index.css';
 import App from './App';
 
+//Action types
 const INCREMENT_COUNTER = 'INCREMENT_COUNTER'
 const DECREMENT_COUNTER = 'DECREMENT_COUNTER'
+
+//Actions
+const inc = ()=>({
+    type: INCREMENT_COUNTER
+})
+const dec = ()=>({
+    type: DECREMENT_COUNTER
+})
+
+//Reducer
 const counterReducer = (state = 0, action) => {
     switch (action.type) {
         case INCREMENT_COUNTER:
             return state + 1
         case DECREMENT_COUNTER:
-            return state - 1
+            return state - 2
         default:
             return state
     }
 }
 
-let store = createStore(counterReducer)
-console.log('state initial', store.getState())
-store.dispatch({
-    type: INCREMENT_COUNTER
-})
-console.log('state after incr. dispatch', store.getState())
+//Store
+const store = createStore(
+    counterReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
-store.dispatch({
-    type: DECREMENT_COUNTER
-})
-console.log('state after decr. dispatch', store.getState())
+window.inc = () => store.dispatch(inc())
 
-store.dispatch({
-    type: DECREMENT_COUNTER
-})
-console.log('state after 2nd decr. dispatch', store.getState())
+window.dec = () => store.dispatch(dec())
 
 
 
