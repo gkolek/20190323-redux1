dfa7351534ffe9f90047d4744923473ab81a2e43
Grzegorz Kolek <gkolek@gazeta.pl>
2019-03-23 10:45:07 +0100

    action types, actions, reducer extracted to separate substore file

--- src/index.js
+++ src/index.js
@@ -2,32 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux'
 
+import {inc, dec, counterReducer} from './store/counter'
+
 import './index.css';
 import App from './App';
 
-//Action types
-const INCREMENT_COUNTER = 'INCREMENT_COUNTER'
-const DECREMENT_COUNTER = 'DECREMENT_COUNTER'
-
-//Actions
-const inc = ()=>({
-    type: INCREMENT_COUNTER
-})
-const dec = ()=>({
-    type: DECREMENT_COUNTER
-})
-
-//Reducer
-const counterReducer = (state = 0, action) => {
-    switch (action.type) {
-        case INCREMENT_COUNTER:
-            return state + 1
-        case DECREMENT_COUNTER:
-            return state - 2
-        default:
-            return state
-    }
-}
 
 //Store
 const store = createStore(
