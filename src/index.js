45bb2f7a0cbcc15f3d284923b43522bc814dbb42
Grzegorz Kolek <gkolek@gazeta.pl>
2019-03-23 09:48:55 +0100

    increase-decrease-counter

--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ import { createStore } from 'redux'
 import './index.css';
 import App from './App';
 
+const INCREMENT_COUNTER = 'INCREMENT_COUNTER'
+const DECREMENT_COUNTER = 'DECREMENT_COUNTER'
 const counterReducer = (state = 0, action) => {
     switch (action.type) {
-        case 'INCREMENT_COUNTER':
+        case INCREMENT_COUNTER:
             return state + 1
+        case DECREMENT_COUNTER:
+            return state - 1
         default:
             return state
     }
@@ -17,9 +21,19 @@ const counterReducer = (state = 0, action) => {
 let store = createStore(counterReducer)
 console.log('state initial', store.getState())
 store.dispatch({
-    type: 'INCREMENT_COUNTER'
+    type: INCREMENT_COUNTER
 })
-console.log('state after dispatch', store.getState())
+console.log('state after incr. dispatch', store.getState())
+
+store.dispatch({
+    type: DECREMENT_COUNTER
+})
+console.log('state after decr. dispatch', store.getState())
+
+store.dispatch({
+    type: DECREMENT_COUNTER
+})
+console.log('state after 2nd decr. dispatch', store.getState())
 
 
 
